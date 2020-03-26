c88742981c123c256b178156844312e3b3e9f79f
anil235476 <aks.dce.007@gmail.com>
2020-03-26 19:40:31 +0530

    adding unit test app and making it cmake based project

---
+++ test_app/MicCaptureWSAAPI.cpp
@@ -0,0 +1,46 @@
+
+#include <iostream>
+
+#include "GetAudio.h"
+#include <cassert>
+#include <chrono>
+
+
+class AudioCaptureTest:public AudioCapture::callback {
+public:
+	AudioCaptureTest() {
+		fopen_s(&ftest, "CAudio.pcm", "wb");
+		assert(ftest);
+	};
+	void onData(const uint8_t* data, int size,
+		int bits_per_sample,
+		int sample_rate,
+		size_t number_of_channels,
+		size_t number_of_frames) override {
+		std::cout << "bits per sample " << bits_per_sample << " sample rate = " << sample_rate << " channel = " << number_of_channels << '\n';
+		fwrite(data, 1, size, ftest);
+	}
+	
+	~AudioCaptureTest() {
+		assert(ftest);
+		fclose(ftest);
+	}
+public:
+	FILE *ftest = nullptr;
+};
+
+int main()
+{
+	AudioCapture::AudioCaptureRaw audioSource{};
+	AudioCaptureTest audioWriter{};
+	audioSource.startCapture(&audioWriter);
+	
+	int close = 0;
+	std::this_thread::sleep_for(std::chrono::seconds(15));
+	
+	audioSource.stopCapture();
+	
+	
+	return 0;
+
+}
